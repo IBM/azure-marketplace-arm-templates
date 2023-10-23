4be12de416e8ee91deeb2c919ddef6e24f538588
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2023-10-23 14:35:58 +1100

    Adds wait for operators function
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- cloud-pak-for-integration/with-ipi/scripts/common.sh
+++ cloud-pak-for-integration/with-ipi/scripts/common.sh
@@ -359,3 +359,79 @@ function download-openshift-installer() {
     fi
 
 }
+
+function wait_for_cluster_operators() {
+# Login to an OpenShift cluster. Must be logged into az cli beforehand and az cli must be in PATH
+# Usage:
+#        wait_for_cluster_operators API_SERVER OCP_USERNAME OCP_PASSWORD BIN_DIR 
+#
+
+    if [[ -z ${1} ]] || [[ -z $API_SERVER ]]; then
+        log-output "ERROR: API_SERVER not passed to function oc-login"
+        exit 1
+    elif [[ ${1} != "" ]]; then
+        API_SERVER=${1}
+    fi
+
+    if [[ -z ${2} ]] || [[ -z $OCP_USERNAME ]]; then
+        log-output "ERROR: OCP_USERNAME not passed to function oc-login"
+        exit 1
+    elif [[ ${2} != "" ]]; then
+        OCP_USERNAME=${2}
+    fi
+
+    if [[ -z ${3} ]] || [[ -z $OCP_PASSWORD ]]; then
+        log-output "ERROR: OCP_PASSWORD not passed to function oc-login"
+        exit 1
+    elif [[ ${3} != "" ]]; then
+        OCP_PASSWORD=${3}
+    fi
+
+    if [[ -z ${4} ]] || [[ -z $BIN_DIR ]]; then
+        BIN_DIR="/usr/local/bin"
+    elif [[ ${4} != "" ]]; then
+        BIN_DIR="${4}"
+    fi
+
+    log-output "INFO: Checking for cluster operator status"
+    # Attempt to login to cluster if not already
+    if ! ${BIN_DIR}/oc status 1> /dev/null 2> /dev/null; then
+        log-output "INFO: Attempting login to OpenShift cluster $API_SERVER"
+
+        # Below loop added to allow authentication service to start on new clusters
+        count=0
+        while ! ${BIN_DIR}/oc login $API_SERVER -u $OCP_USERNAME -p $OCP_PASSWORD --insecure-skip-tls-verify=true 1> /dev/null 2> /dev/null ; do
+            log-output "INFO: Waiting to log into cluster. Waited $count minutes. Will wait up to 15 minutes."
+            sleep 60
+            count=$(( $count + 1 ))
+            if (( $count > 15 )); then
+                log-output "ERROR: Timeout waiting to log into cluster"
+                exit 1;    
+            fi
+        done
+        log-output "INFO: Successfully logged into cluster $API_SERVER"
+        existing_login="no"
+    else
+        log-output "INFO: Already logged into cluster"
+        existing_login="yes"
+    fi
+
+    # Wait for cluster operators to be available
+    count=0
+    while ${BIN_DIR}/oc get clusteroperators | awk '{print $4}' | grep True; do
+        log-output "INFO: Waiting on cluster operators to be availabe. Waited $count minutes. Will wait up to 30 minutes."
+        sleep 60
+        count=$(( $count + 1 ))
+        if (( $count > 30 )); then
+            log-output "ERROR: Timeout waiting for cluster operators to be available"
+            exit 1;
+        fi
+    done
+    log-output "INFO: Cluster operators are ready"
+
+    # Log out of cluster to allow secure login
+    if [[ $existing_login == "no" ]]; then
+        log-output "INFO: Logging out of temporary cluster login"
+        oc logout 1> /dev/null 2> /dev/null
+    fi
+}
