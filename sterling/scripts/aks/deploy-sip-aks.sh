38842a26252c605aa80136ee3f0ecc8004922f9e
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2024-02-28 15:27:01 +1100

    Multiple script updates
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- sterling/scripts/aks/deploy-sip-aks.sh
+++ sterling/scripts/aks/deploy-sip-aks.sh
@@ -9,6 +9,8 @@ log-info "Script started"
 # Check required parameters
 if [[ -z $RESOURCE_GROUP ]]; then log-error "RESOURCE_GROUP not defined"; exit 1; fi
 if [[ -z $IBM_ENTITLEMENT_KEY ]]; then log-error "IBM_ENTITLEMENT_KEY not defined"; exit 1; fi
+if [[ -z $TRUSTSTORE_PASSWORD ]]; then log-error "TRUSTSTORE_PASSWORD not defined"; exit 1; fi
+if [[ -z $DOMAIN_NAME ]]; then log-error "DOMAIN_NAME is not defined"; fi
 
 # Set default values
 if [[ -z $TMP_DIR ]]; then TMP_DIR="$(pwd)"; fi
@@ -26,6 +28,10 @@ if [[ -z $IMAGE_LIST_RH_URL ]]; then IMAGE_LIST_RH_URL="${BASE_URI}/${BRANCH}/st
 if [[ -z $IMAGE_LIST_SIP_URL ]]; then IMAGE_LIST_SIP_URL="${BASE_URI}/${BRANCH}/sterling/scripts/common/${IMAGE_LIST_SIP_FILENAME}"; fi
 if [[ -z $SC_NAME ]]; then SC_NAME="azurefile"; fi
 if [[ -z $PVC_NAME ]]; then PVC_NAME="sip1-pvc"; fi
+if [[ -z $LICENSE ]]; then LICENSE="decline"; fi
+if [[ -z $CERT_MANAGER_VERSION ]]; then CERT_MANAGER_VERSION="v1.14.3"; fi
+if [[ -z $PVC_SIZE ]]; then PVC_SIZE="10Gi"; fi
+if [[ -z $CP_REPO_BASE ]]; then CP_REPO_BASE="cp/ibm-oms-enterprise"; fi
 
 # Download the image lists
 if [[ -f ${WORKSPACE_DIR}/${IMAGE_LIST_RH_FILENAME} ]]; then
@@ -89,11 +95,44 @@ if [[ -z $(which kubectl) ]] || [[ -z $(which kubelogin) ]]; then
     log-info "Installing kubectl and kubelogin CLI tools"
     if [[ $(whoami) == "root" ]]; then
         az aks install-cli
+        if (( $? != 0 )); then
+            log-error "Unable to install kubectl and kubelogin"
+            exit 1
+        fi
     else
         sudo az aks install-cli
+        if (( $? != 0 )); then
+            log-error "Unable to install kubectl and kubelogin"
+            exit 1
+        fi
     fi
 else
     log-info "kubectl and kubelogin CLI tools already installed"
+fi
+
+# Install the certificate manager tool if not already installed
+if [[ -z $(which cmctl) ]]; then
+    log-info "Installing certificate manager control CLI tool"
+    OS=$(uname | awk '{print tolower($0)}')
+    ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
+    curl -fsSL -o ${TMP_DIR}/cmctl.tar.gz https://github.com/cert-manager/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cmctl-$OS-$ARCH.tar.gz
+    if (( $? != 0 )); then
+        log-error "Unable to download cmctl from https://github.com/cert-manager/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cmctl-$OS-$ARCH.tar.gz"
+        exit 1
+    else
+        tar xzf ${TMP_DIR}/cmctl.tar.gz
+        if (( $? != 0 )); then
+            log-error "Unable to untar file ${TMP_DIR}/cmctl.tar.gz"
+            exit 1
+        fi
+        mv cmctl ${BIN_DIR}
+        if (( $? != 0 )); then
+            log-error "Unable to move file cmctl to ${BIN_DIR}"
+            exit 1
+        fi
+    fi
+else
+    log-info "Certificate manager control tool already installed"
 fi
 
 # Install the operatorSDK if not already
@@ -122,7 +161,7 @@ if [[ -z $ACR_NAME ]]; then
         log-error "More than one ACR visible. Please specify desired ACR in environment parameter"
         exit 1
     else
-        ACR_NAME="${ACRS[0]}"
+        ACR_NAME="$(echo ${ACRS[0]} | awk -F. '{print $1}')"
     fi
 fi
 log-info "Azure container registry is set to $ACR_NAME"
@@ -144,7 +183,13 @@ log-info "Logging into AKS Cluster ${AKS_CLUSTER} in resource group $RESOURCE_GR
 az aks get-credentials -n $AKS_CLUSTER -g $RESOURCE_GROUP
 
 # Add OLM to the cluster
-operator-sdk olm install
+kubectl get pods -n olm | grep olm-operator 2>&1
+if (( $? != 0 )); then
+    log-info "Installing OperatorSDK OLM"
+    operator-sdk olm install
+else
+    log-info "OperatorSDK OLM already installed"
+fi
 
 # Create the operator namespace
 if [[ -z $(kubectl get ns | grep ${OPERATOR_NAMESPACE} ) ]]; then
@@ -306,45 +351,205 @@ fi
 # Upload images to the Azure Container Registry
 log-info "Importing required Red Hat images to the Azure Container Registry"
 while read image; do
-    log-info "Importing ubi8/$image"
-    az acr import \
-        --name $ACR_NAME \
-        --source registry.access.redhat.com/ubi8/$image \
-        --image ubi8/$image
-    if (( $? != 0 )); then
-        log-error "Unable to import image ubi8/$image"
-        exit 1
+    REPO_NAME="ubi8/$(echo $image | awk -F":" '{print $1}')"
+    if [[ -z $(az acr repository list --name $ACR_NAME -o tsv | grep $REPO_NAME) ]]; then
+        log-info "Importing ubi8/$image to $ACR_NAME"
+        az acr import \
+            --name $ACR_NAME \
+            --source registry.access.redhat.com/ubi8/$image \
+            --image ubi8/$image
+        if (( $? != 0 )); then
+            log-error "Unable to import image ubi8/$image to $ACR_NAME"
+            exit 1
+        else
+            log-info "Successfully imported image ubi8/$image to $ACR_NAME"
+        fi
     else
-        log-info "Successfully imported image ubi8/$image"
+        log-info "Image ubi8/$image already exists in $ACR_NAME repository"
     fi
 done < ${WORKSPACE_DIR}/${IMAGE_LIST_RH_FILENAME}
 
 log-info "Importing required SIP images to the Azure Container Registry"
 while read image; do
-    log-info "Importing cp/ibm-oms-enterprise/$image"
-    az acr import \
-        --name $ACR_NAME \
-        --source cp.icr.io/cp/ibm-oms-enterprise/$image \
-        --image cp/ibm-oms-enterprise/$image \
-        --username cp \
-        --password $IBM_ENTITLEMENT_KEY
-    if (( $? != 0 )); then
-        log-error "Unable to import image cp/ibm-oms-enterprise/$image"
-        exit 1
+    REPO_NAME="${CP_REPO_BASE}/$(echo $image | awk -F":" '{print $1}')"
+    if [[ -z $(az acr repository list --name $ACR_NAME -o tsv | grep $REPO_NAME) ]]; then
+        log-info "Importing ${CP_REPO_BASE}/$image to $ACR_NAME"
+        az acr import \
+            --name $ACR_NAME \
+            --source cp.icr.io/${CP_REPO_BASE}/$image \
+            --image ${$CP_REPO_BASE}/$image \
+            --username cp \
+            --password $IBM_ENTITLEMENT_KEY
+        if (( $? != 0 )); then
+            log-error "Unable to import image ${CP_REPO_BASE}/$image to $ACR_NAME"
+            exit 1
+        else
+            log-info "Successfully imported image ${CP_REPO_BASE}/$image to $ACR_NAME"
+        fi
     else
-        log-info "Successfully imported image cp/ibm-oms-enterprise/$image"
+        log-info "Image ${CP_REPO_BASE}/$image already exists in $ACR_NAME repository"
     fi
 done < ${WORKSPACE_DIR}/${IMAGE_LIST_SIP_FILENAME}
 
-#########
-# Create JWT issuer cert
+# Create the certificate manager CRD
+if [[ -z $(kubectl get crds | grep certificatemanagers) ]]; then
+    log-info "Installing the certificate manager operator"
+    kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml
+    if (( $? != 0 )); then
+        log-error "Unable to install certificate manager operator"
+        exit 1
+    else
+        while [[ -z $(kubectl get deployments -n cert-manager | grep "cert-manager " | grep "1/1") ]] \
+            && [[ -z $(kubectl get deployments -n cert-manager | grep "cert-manager-cainjector" | grep "1/1") ]] \
+            && [[ -z $(kubectl get deployments -n cert-manager | grep "cert-manager-webhook" | grep "1/1") ]]; do
+            log-info "Waiting for certificate manager to initialize"
+            i=$(( $i + 1))
+            if (( $i > 10 )); then
+                log-error "Timeout waiting for certificate manager to initialize"
+                exit 1
+            fi
+            sleep 30
+        done
+        log-info "Certificate manager installed and running"
+    fi
+else
+    log-info "Certificate Manager custom resource definition already exists"
+fi
 
 # Deploy SIP instance
-if [[ -z $(kubectl get sipenvironment -n $SIP_NAMESPACE | grep sip ) ]]; then
+if [[ -z $(kubectl get sipenvironment -n $SIP_NAMESPACE | grep " sip " ) ]]; then
     if [[ $LICENSE == "accept" ]]; then
-        if [[ -z $(kubectl get pvc -n $SIP_NAMESPACE) | grep $PVC_NAME ]]; then
+
+        # Create JWT issuer cert
+        HOSTNAME="sipservice-${SIP_NAMESPACE}.$DOMAIN_NAME"
+
+        if [[ -z $(kubectl get certificatemanager -n $SIP_NAMESPACE | grep "ingress-cert ") ]]; then
+            log-info "Creating ingress certificate"
+            cat << EOF | kubectl create -f -
+apiVersion: apps.oms.gateway.ibm.com/v1beta1
+kind: CertificateManager
+metadata:
+  name: ingress-cert
+  namespace: ${SIP_NAMESPACE}
+spec:
+  expiryDays: 365
+  hostName: ${HOSTNAME}
+EOF
+            if (( $? != 0 )); then
+                log-error "Unable to create ingress certificate"
+                exit 1
+            else
+                log-info "Ingress certificate created"
+            fi
+        else
+            log-info "Ingress certificate already exists"
+        fi
+
+        # Create the truststore password
+        ######### The below needs to be augmented and/or changed for external middleware services if utilised.
+        log-info "Creating / updating truststore secret"
+        cat << EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: sip-secret
+  namespace: $SIP_NAMESPACE
+type: Opaque
+stringData:
+  truststore_password: ${TRUSTSTORE_PASSWORD}
+  cassandra_username: ${CASSANDRA_USERNAME}
+  cassandra_password: ${CASSANDRA_PASSWORD}
+  elasticsearch_username: ${ELASTICSEARCH_USERNAME}
+  elasticsearch_password: ${ELASTICSEARCH_PASSWORD}
+  kafka_security_protocol: ${KAFKA_SECURITY_PROTOCOL}
+  kafka_sasl_jaas_config: ${KAFKA_SASL_JAAS_CONFIG}
+  kafka_user: ${KAFKA_USER}
+  kafka_password: ${KAFKA_PASSWORD}
+  kafka_sasl_mechanism: ${KAFKA_SASL_MECHANISM}
+EOF
+        if (( $? != 0 )); then
+            log-error "Unable to create or update truststore secret"
+            exit 1
+        else
+            log-info "Successfully created / updated truststore secret"
+        fi
+
+        # Create the persistant volume
+        if [[ -z $(kubectl get pvc -n $SIP_NAMESPACE | grep $PVC_NAME ) ]]; then
             log-info "Creating the persistent volume $PVC_NAME in namespace $SIP_NAMESPACE"
-            
+            cat << EOF | kubectl create -f -
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: $PVC_NAME
+  namespace: $SIP_NAMESPACE
+spec:
+  accessModes:
+    - ReadWriteMany
+  volumeMode: Filesystem
+  storageClassName: $SC_NAME
+  resources:
+    requests:
+      storage: ${PVC_SIZE}
+EOF
+            if (( $? != 0 )); then
+                log-error "Unable to create PVC $PVC_NAME in namespace $SIP_NAMESPACE"
+                exit 1
+            else
+                log-info "Created PVC $PVC_NAME in namespace $SIP_NAMESPACE"
+            fi
+
+            log-info "Running job to mount volume and force PV creation"
+            # Remove any existing job
+            if [[ $(kubectl get jobs -n $SIP_NAMESPACE | grep volume-pod ) ]]; then
+                log-info "Deleting existing volume-pod job in namespace $SIP_NAMESPACE"
+                kubectl delete job -n $SIP_NAMESPACE volume-pod
+                if (( $? != 0 )); then
+                    log-error "Unable to delete volume-pod job in namespace $SIP_NAMESPACE"
+                    exit 1
+                else
+                    log-info "Deleted volume-pod job in namespace $SIP_NAMESPACE"
+                fi
+            fi
+            cat << EOF | kubectl create -f -
+kind: Job
+apiVersion: batch/v1
+metadata: 
+  name: volume-pod
+  namespace: $SIP_NAMESPACE
+spec:
+  template:
+    spec:
+      volumes:
+        - name: sip-volume
+          persistentVolumeClaim:
+            claimName: $PVC_NAME
+      containers:
+        - name: nginx
+          image: nginx:latest
+          command: [ "/bin/bash", "-c", "--" ]
+          args: [ "echo done" ]
+          volumeMounts:
+            - name: sip-volume
+              mountPath: /mnt
+      restartPolicy: OnFailure
+  backoffLimit: 4
+EOF
+            if (( $? != 0 )); then
+                log-error "Unable to create batch job to mount volume"
+                exit 1
+            else
+                while [[ -z $(kubectl get job volume-pod -n $SIP_NAMESPACE | grep "1/1") ]]; do
+                    log-info "Waiting for volume-pod job in namespace $SIP_NAMESPACE to complete"
+                    i=$(( $i + 1 ))
+                    if (( $i > 10 )); then
+                        log-error "Timeout waiting for volume-pod job in namespace $SIP_NAMESPACE to complete"
+                        exit 1
+                    fi
+                    sleep 30
+                done
+                log-info "Job volume-pod completed in namespace $SIP_NAMESPACE"
+            fi
         else
             log-info "PVC $PVC_NAME already exists in namespace $SIP_NAMESPACE"
         fi        
@@ -355,7 +560,7 @@ apiVersion: apps.sip.ibm.com/v1beta1
 kind: SIPEnvironment
 metadata:
   name: sip
-  namespace: $NAMESPACE
+  namespace: $SIP_NAMESPACE
   annotations:
     internal.sip.ibm.com/skip-ibm-entitlement-key-check: 'yes'
 spec:
@@ -400,7 +605,7 @@ spec:
       createDevInstance: {}
   common:
     ingress:
-      host: $FQDN
+      host: $HOSTNAME
       ssl:
        enabled: true
        identitySecretName: ingress-cert
@@ -408,7 +613,7 @@ spec:
     imagePullSecrets:
     - name: ibm-entitlement-key
     - name: acr-secret
-    repository: $ACR_SERVER/cp/ibm-oms-enterprise
+    repository: $ACR_NAME/cp/ibm-oms-enterprise
     tag: $SIP_VERSION
   storage:
     accessMode: ReadWriteMany
@@ -424,6 +629,38 @@ EOF
 
             ###########
             log-info "Successfully created SIP instance in namespace $SIP_NAMESPACE"
+        fi
+
+        # Create ingress
+        if [[ -z $() ]]; then
+            log-info "Creating ingress instance for SIP instance"
+            cat << EOF | kubectl apply -f -
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: sip-ingress
+  namespace: ${NAMESPACE}
+  annotations:
+    nginx.ingress.kubernetes.io/backend-protocol: HTTPS
+spec:
+  ingressClassName: webapprouting.kubernetes.azure.com
+  tls:
+  - hosts:
+    - ${HOSTNAME}
+  rules:
+  - host: ${HOSTNAME}
+    http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service: 
+            name: sip
+            port: 
+              number: 8300
+EOF
+        else
+            log-info "Ingress instance already exists"
         fi
     else
         log-info "License not accepted. Instance not created"
