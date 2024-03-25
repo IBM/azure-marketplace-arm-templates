eda2b2a225629dd752a1fcbdd04822a7d536bdd8
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2024-03-25 11:22:57 +1100

    Fixes typo in description
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- sterling/2024-03/sip/aks/deploy-sip-aks.sh
+++ sterling/2024-03/sip/aks/deploy-sip-aks.sh
@@ -153,7 +153,7 @@ fi
 
 # Install the operatorSDK if not already
 if [[ -z $(which operator-sdk) ]]; then
-    log-info "Installing the Oerator-SDK CLI tool"
+    log-info "Installing the Operator-SDK CLI tool"
     export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
     export OS=$(uname | awk '{print tolower($0)}')
     export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.32.0
