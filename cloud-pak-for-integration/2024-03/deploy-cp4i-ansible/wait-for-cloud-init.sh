7a991be981b17755f1e4552562bf23ea83b7da8e
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2024-05-23 16:14:37 +1000

    Adds wait for cloud-init and fixes custom data
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

---
+++ cloud-pak-for-integration/2024-03/deploy-cp4i-ansible/wait-for-cloud-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Wait for cloud-init to finish
+count=0
+while [[ $(/usr/bin/ps xua | grep cloud-init | grep -v grep) ]]; do
+    echo "INFO: Waiting for cloud init to finish. Waited $count minutes. Will wait 15 mintues."
+    sleep 60
+    count=$(( $count + 1 ))
+    if (( $count > 15 )); then
+        echo "ERROR: Timeout waiting for cloud-init to finish"
+        exit 1;
+    fi
+done
