82b7562b1e91a1475cc9deeea56be91ca889948f
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2024-05-24 14:38:39 +1000

    Updates wait for cloud init and other minor changes
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- cloud-pak-for-integration/2024-03/deploy-cp4i-ansible/wait-for-cloud-init.sh
+++ cloud-pak-for-integration/2024-03/deploy-cp4i-ansible/wait-for-cloud-init.sh
@@ -2,8 +2,8 @@
 
 # Wait for cloud-init to finish
 count=0
-while [[ $(/usr/bin/ps xua | grep cloud-init | grep -v grep) ]]; do
-    echo "INFO: Waiting for cloud init to finish. Waited $count minutes. Will wait 15 mintues."
+until [[ $(/usr/bin/ps xua | grep cloud-init | grep -v grep) != "" ]]; do
+    echo "INFO: Waiting for cloud init to finish. Waited $count minutes. Will wait 15 minutes."
     sleep 60
     count=$(( $count + 1 ))
     if (( $count > 15 )); then
