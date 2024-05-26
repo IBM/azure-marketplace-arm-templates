7829ffe271650d8104a40816169f73b722e41365
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2024-05-27 09:27:53 +1000

    Specifies ansible install dir and wait for ansible to install
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- cloud-pak-for-integration/2024-03/deploy-cp4i-ansible/wait-for-cloud-init.sh
+++ cloud-pak-for-integration/2024-03/deploy-cp4i-ansible/wait-for-cloud-init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Wait for cloud-init to finish
+# Wait for cloud-init to finish installing ansible
 count=0
-until [[ $(/usr/bin/ps xua | grep cloud-init | grep -v grep) == "" ]]; do
-    echo "INFO: Waiting for cloud init to finish. Waited $count minutes. Will wait 15 minutes."
+while [[ ! -f /usr/local/bin/ansible-playbook ]]; do
+    echo "INFO: Waiting for cloud init to finish installing ansible. Waited $count minutes. Will wait 15 minutes."
     sleep 60
     count=$(( $count + 1 ))
     if (( $count > 15 )); then
-        echo "ERROR: Timeout waiting for cloud-init to finish"
+        echo "ERROR: Timeout waiting for cloud-init to install ansible"
         exit 1;
     fi
 done
