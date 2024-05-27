35becc63007f4287bfc38b12fd9c3ce6a48bab84
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2024-05-27 11:46:52 +1000

    Updates to ansible 2.16.3
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- cloud-pak-for-integration/2024-03/deploy-cp4i-ansible/wait-for-cloud-init.sh
+++ cloud-pak-for-integration/2024-03/deploy-cp4i-ansible/wait-for-cloud-init.sh
@@ -2,11 +2,11 @@
 
 # Wait for cloud-init to finish installing ansible
 count=0
-while [[ ! -f /usr/bin/ansible-playbook ]]; do
-    echo "INFO: Waiting for cloud init to finish installing ansible. Waited $count minutes. Will wait 15 minutes."
+while [[ ! -f /usr/local/bin/ansible-playbook ]]; do
+    echo "INFO: Waiting for cloud init to finish installing ansible. Waited $count minutes. Will wait 30 minutes."
     sleep 60
     count=$(( $count + 1 ))
-    if (( $count > 15 )); then
+    if (( $count > 30 )); then
         echo "ERROR: Timeout waiting for cloud-init to install ansible"
         exit 1;
     fi
