4a4703c3fd57a0db9dfc683c07c55f44d48c99f4
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2025-01-31 14:58:02 +1100

    Adds pwsh script and extension
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- webMethods/2025-01/deploy-webMethods/deploy-vm-win.ps1
+++ webMethods/2025-01/deploy-webMethods/deploy-vm-win.ps1
@@ -1,15 +1,59 @@
 # Script to download and install webMethod on a Windows VM
+param (
+    $jsonString
+)
 
-##### TEST
-Write-Host "Hello World!"
-
-# Parse the input file
+# Convert input parameter to object
+try {
+    $parameters = $jsonString | ConvertFrom-Json
+} catch {
+    Write-Error "Error parsing JSON $_"
+}
 
 # Download webMethod installer binary
+$webMethodsInstaller = ".\$($parameters.installerName)"
+if (-not(Test-Path -path $webMethodsInstaller)) {
+    Write-Output "Attempting to download webMethod Installer binary"
+    [DownloadWithRetry]::DoDownloadWithRetry($($parameters.installerURL),5, 10, $null,  $webMethodsInstaller, $false)
+} else {
+    Write-Output "webMethod installer already exists on server"
+}
 
 # Create the script file
+$scriptFile = ".\script-file"
+if (-not(Test-Path -path $scriptFile)) {
+    Write-Output "Creating script file"
+    New-Item -Path $scriptFile
+    Add-Content -Path $scriptFile "ServerURL=$($parameters.wmServerUrl)"
+    Add-Content -Path $scriptFile "selectedFixes=$($parameters.selectedFixes)"
+    Add-Content -Path $scriptFile "Username=$($parameters.emailAddress)"
+    Add-Content -Path $scriptFile "StartMenuFolder=Software AG"
+    Add-Content -Path $scriptFile "HostName=$env:computername"
+    Add-Content -Path $scriptFile "InstallProducts=$($parameters.installProducts)"
+    Add-Content -Path $scriptFile "Password=$($parameters.entitlementKey)"
+    Add-Content -Path $scriptFile "InstallDir=$($parameters.installDirectory)"
+} else {
+    Write-Output "Script file already exists"
+}
 
 # Run the installer
+if ($($parameters.licenseAccepted )) {
+    Write-Output "Attempting to install base webMethods"
+    try {
+        cmd.exe /c $webMethodsInstaller -readScript $scriptFile 
+    } catch {
+        Write-Error "Failed to install webMethods"
+    }
+} else {
+    Write-Output "License not accepted. Not installing."
+}
+
+# Copy the installer to the install dir
+$webMethodDirectory = $($parameters.installDirectory).Replace("\:",":").Replace("\\","\") 
+Copy-Item -Path $webMethodsInstaller -Destination "$webMethodDirectory\install\bin\"
+
+# Clean up the script file
+Remove-Item -Path $scriptFile
 
 class DownloadWithRetry {
     static [string] DoDownloadWithRetry([string] $uri, [int] $maxRetries, [int] $retryWaitInSeconds, [string] $authToken, [string] $outFile, [bool] $metadata) {
@@ -66,3 +110,5 @@ class DownloadWithRetry {
         return ""
     }
 }
+
+ 
